f926372e909f435f8726e60b83ce0665cae3ece9
LisaHJung <lisahjung@gmail.com>
2020-04-23 20:19:41 -0600

    Lisa's notes on Jared's api lessson

--- db/migrate/20200422222113_create_coffees.rb
+++ db/migrate/20200422222113_create_coffees.rb
@@ -10,3 +10,19 @@ class CreateCoffees < ActiveRecord::Migration[6.0]
     end
   end
 end
+
+# any model that is dependent upon the other, you have to have t.references and include the foreign_key to be true. 
+# references column then associate it with other database table. 
+#null- will not allow an instance of a coffee to be created if it does not have the coffee shop id referenced as part of its parameters. 
+
+# rails g resource coffee (singular)
+# followed steps as you did for coffee shop. 
+# whenever you have dependent resource, declare independent resource FIRST - BEST PRACTICE
+# ORDER MATTERS
+
+# M:N
+  # independent and join is now dependent and coffee and coffee shop
+  # two things that have to have M:N relationship first- order for these do not matter
+  # join- dependent
+    #rails g resource
+  
